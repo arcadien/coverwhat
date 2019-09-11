774cada51c36f55f6d042fab4293e63fcf14bf46
Aurelien Labrosse <aurelien@skima.fr>
2019-09-11 23:43:51 +0200

    chore: More isolation of AVR third party libraries

--- scripts/init-avr-linux.sh
+++ scripts/init-avr-linux.sh
@@ -4,6 +4,6 @@ cmake  ../ \
 -DMCU_SPEED=16000000U \
 -DAVR_MCU=$MCU \
 -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_TOOLCHAIN_FILE=../third_party/cmake-avr/generic-gcc-avr.cmake
+-DCMAKE_TOOLCHAIN_FILE=../third_party/avr/cmake-avr/generic-gcc-avr.cmake
 
 
