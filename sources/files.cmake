2471dda72a01e52c031621d52985adacb8b6db18
Aurelien Labrosse <aurelien.labrosse@skima.fr>
2019-10-11 18:58:29 +0200

    chore(build): Do not include simulator code in AVR build

--- sources/files.cmake
+++ sources/files.cmake
@@ -16,7 +16,6 @@ set(LIB_H
   ./include/api/Actor.h
   ./include/api/Entity.h
   ./include/api/ITransport.h
-  ./include/hardware/simulator/Simulator.h
   ./include/actors/Player.h
   ./include/actors/Weapon.h
   ./include/transports/Software.h
@@ -28,7 +27,17 @@ set(LIB_CPP
   ./src/api/IHardware.cpp
   ./src/api/Actor.cpp
   ./src/api/ITransport.cpp
-  ./src/hardware/simulator/Simulator.cpp
   ./src/weapons/Vampire.cpp
   ./src/transports/Software.cpp
 )
+
+if(NOT AVR)
+  list(APPEND LIB_CPP
+    ./src/hardware/simulator/Simulator.cpp
+  )
+
+  list(APPEND LIB_H
+    ./include/hardware/simulator/Simulator.h
+  )
+endif()
+
