b123fba0b53f8fa879deba268792baa115ad2236
Aurelien Labrosse <aurelien@skima.fr>
2019-10-21 22:03:40 +0200

    feat: Add more Action types

--- sources/include/api/Action.h
+++ sources/include/api/Action.h
@@ -21,13 +21,14 @@
  * milliseconds.
  */
 class Action {
-public:
+ public:
   /*
    * Initial value which can take an Action
    *
    *
    */
   enum class Amount : uint8_t {
+    AMOUNT_0 = 0,
     AMOUNT_1 = 1,
     AMOUNT_5 = 5,
     AMOUNT_10 = 10,
@@ -38,45 +39,42 @@ public:
     AMOUNT_FULL = 255
   };
 
-  enum Type {
-    DAMAGE,
-    HEAL,
-  };
+  enum Type { DAMAGE, HEAL, STUN, RESURRECT, NONE };
 
   Action(Type const &type, Entity::Tag const &entityTag, Amount const &amount,
          uint8_t cooldown)
-      : _type(type), _entityTag(entityTag), _amount(amount),
-        _coolDown(cooldown) {
-  }
+      : _type(type),
+        _entityTag(entityTag),
+        _amount(amount),
+        _coolDown(cooldown) {}
 
-  Entity::Tag const &GetTargetEntityTag() const {
-    return _entityTag;
-  }
+  Entity::Tag const &GetTargetEntityTag() const { return _entityTag; }
 
-  Amount GetAmount() const {
-    return _amount;
-  }
+  Amount GetAmount() const { return _amount; }
 
-  uint8_t GetCoolDownMs() const {
-    return _coolDown;
-  }
+  uint8_t GetCoolDownMs() const { return _coolDown; }
 
   uint16_t Process(uint16_t currentValue) const {
     uint16_t result = currentValue;
     switch (_type) {
-    case (DAMAGE):
-      result -= (uint16_t)_amount;
-      break;
-    case (HEAL):
-      result += (uint16_t)_amount;
-      break;
+      case (DAMAGE):
+        result -= (uint16_t)_amount;
+        break;
+      case (HEAL):
+        result += (uint16_t)_amount;
+        break;
+      case (RESURRECT):
+      case (STUN):
+      case (NONE):
+        break;
     }
     return result;
   }
 
-private:
+ private:
   Type _type;
-  Entity::Tag const &_entityTag;
+  Entity::Tag _entityTag;
   Amount _amount;
   uint8_t _coolDown;
 };
+
