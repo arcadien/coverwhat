0b510f1017a3eb6b5f16ae67091acb616b03d6a8
Aurelien Labrosse <aurelien@skima.fr>
2019-10-22 22:18:37 +0200

    test: Add test for Simulator

--- software/tests/SimulatorTest.cpp
+++ software/tests/SimulatorTest.cpp
@@ -13,21 +13,23 @@ void setUp(void) {}
 
 void tearDown(void) {}
 
-void Expect_hardware_to_be_clocked();
-void Expect_hardware_to_have_a_primary_action();
-void Expect_hardware_to_have_a_secondary_action();
-void Expect_hardware_to_be_able_to_sleep_for_some_ms();
+void Expect_simulator_to_be_clocked();
+void Expect_simulator_to_have_a_primary_action();
+void Expect_simulator_to_have_a_secondary_action();
+void Expect_simulator_to_be_able_to_sleep_for_some_ms();
+void Expect_simulator_to_wait_10ms_during_waitForEvent();
 
 int main(int, char **) {
   UNITY_BEGIN();
-  RUN_TEST(Expect_hardware_to_be_able_to_sleep_for_some_ms);
-  RUN_TEST(Expect_hardware_to_be_clocked);
-  RUN_TEST(Expect_hardware_to_have_a_primary_action);
-  RUN_TEST(Expect_hardware_to_have_a_secondary_action);
+  RUN_TEST(Expect_simulator_to_be_able_to_sleep_for_some_ms);
+  RUN_TEST(Expect_simulator_to_be_clocked);
+  RUN_TEST(Expect_simulator_to_have_a_primary_action);
+  RUN_TEST(Expect_simulator_to_have_a_secondary_action);
+  RUN_TEST(Expect_simulator_to_wait_10ms_during_waitForEvent);
   return UNITY_END();
 }
 
-void Expect_hardware_to_be_clocked() {
+void Expect_simulator_to_be_clocked() {
   // extended Simulator class which
   // count the ticks
   class TestHw : public HardwareType {
@@ -44,19 +46,37 @@ void Expect_hardware_to_be_clocked() {
   TEST_ASSERT(hw.tickCount > 25);
 }
 
-void Expect_hardware_to_have_a_primary_action() {
+void Expect_simulator_to_wait_10ms_during_waitForEvent() {
+  HardwareType hw;
+  hw.Setup();
+  unsigned long begin;
+  unsigned long end;
+  begin = hw.Millis();
+
+  hw.WaitForEvent();
+
+  end = hw.Millis();
+  int elapsed_ms = end - begin;
+  hw.Stop();
+
+  // sometimes rounding leads to 251ms so
+  // add a little tolerance here
+  TEST_ASSERT_INT_WITHIN(1, 10, elapsed_ms);
+}
+
+void Expect_simulator_to_have_a_primary_action() {
   HardwareType hw;
   hw.TriggerPrimaryAction();
   TEST_ASSERT_EQUAL(1, hw.PrimaryActionCount());
 }
 
-void Expect_hardware_to_have_a_secondary_action() {
+void Expect_simulator_to_have_a_secondary_action() {
   HardwareType hw;
   hw.TriggerSecondaryAction();
   TEST_ASSERT_EQUAL(1, hw.SecondaryActionCount());
 }
 
-void Expect_hardware_to_be_able_to_sleep_for_some_ms() {
+void Expect_simulator_to_be_able_to_sleep_for_some_ms() {
   HardwareType hw;
 
   hw.Setup();
