5cc423844656e4bff720a69856dfd772620cee23
Aurelien Labrosse <aurelien.labrosse@gmail.com>
2019-10-26 10:47:25 +0000

    chore(coverage): Raise test coverage of RGBColor

--- software/tests/RGBColorTest.cpp
+++ software/tests/RGBColorTest.cpp
@@ -1,29 +1,57 @@
 #include <tools/RGBColor.h>
 #include <unity.h>
 
-// Test various color panel for
-// HSV values
 void Expect_hsv_value_to_zero_always_produce_black();
 void Expect_hsv_hue_0_to_be_red();
-void Expect_hsv_hue_240_to_be_blue();
+void Expect_hsv_hue_60_to_be_yellow();
 void Expect_hsv_hue_120_to_be_green();
-void Expect_hsv_saturation_max_value_to_be_100();
+void Expect_hsv_hue_180_to_be_cyan();
+void Expect_hsv_hue_240_to_be_blue();
+void Expect_hsv_hue_300_to_be_violet();
 
 int main(int, char **) {
   UNITY_BEGIN();
-  RUN_TEST(Expect_hsv_saturation_max_value_to_be_100);
   RUN_TEST(Expect_hsv_value_to_zero_always_produce_black);
   RUN_TEST(Expect_hsv_hue_0_to_be_red);
-  RUN_TEST(Expect_hsv_hue_240_to_be_blue);
+  RUN_TEST(Expect_hsv_hue_60_to_be_yellow);
   RUN_TEST(Expect_hsv_hue_120_to_be_green);
+  RUN_TEST(Expect_hsv_hue_180_to_be_cyan);
+  RUN_TEST(Expect_hsv_hue_240_to_be_blue);
+  RUN_TEST(Expect_hsv_hue_300_to_be_violet);
   return UNITY_END();
 }
 void tearDown() {}
 void setUp() {}
 
-void Expect_hsv_saturation_max_value_to_be_100() {
+void Expect_hsv_hue_0_to_be_red() {
   RGBColor color;
-  color.FromHSV(240, 101, 255);
+  color.FromHSV(0, 255, 255);
+  TEST_ASSERT_EQUAL_INT(255, color.red);
+  TEST_ASSERT_EQUAL_INT(0, color.green);
+  TEST_ASSERT_EQUAL_INT(0, color.blue);
+}
+
+void Expect_hsv_hue_60_to_be_yellow() {
+  RGBColor color;
+  color.FromHSV(60, 255, 255);
+  TEST_ASSERT_EQUAL_INT(255, color.red);
+  TEST_ASSERT_EQUAL_INT(255, color.green);
+  TEST_ASSERT_EQUAL_INT(0, color.blue);
+}
+
+void Expect_hsv_hue_120_to_be_green() {
+  RGBColor color;
+  color.FromHSV(120, 255, 255);
+  TEST_ASSERT_EQUAL_INT(0, color.red);
+  TEST_ASSERT_EQUAL_INT(255, color.green);
+  TEST_ASSERT_EQUAL_INT(0, color.blue);
+}
+void Expect_hsv_hue_180_to_be_cyan() {
+  RGBColor color;
+  color.FromHSV(180, 255, 255);
+  TEST_ASSERT_EQUAL_INT(0, color.red);
+  TEST_ASSERT_EQUAL_INT(255, color.green);
+  TEST_ASSERT_EQUAL_INT(255, color.blue);
 }
 
 void Expect_hsv_hue_240_to_be_blue() {
@@ -34,20 +62,12 @@ void Expect_hsv_hue_240_to_be_blue() {
   TEST_ASSERT_EQUAL_INT(255, color.blue);
 }
 
-void Expect_hsv_hue_120_to_be_green() {
+void Expect_hsv_hue_300_to_be_violet() {
   RGBColor color;
-  color.FromHSV(120, 255, 255);
-  TEST_ASSERT_EQUAL_INT(0, color.red);
-  TEST_ASSERT_EQUAL_INT(255, color.green);
-  TEST_ASSERT_EQUAL_INT(0, color.blue);
-}
-
-void Expect_hsv_hue_0_to_be_red() {
-  RGBColor color;
-  color.FromHSV(0, 255, 255);
+  color.FromHSV(300, 255, 255);
   TEST_ASSERT_EQUAL_INT(255, color.red);
   TEST_ASSERT_EQUAL_INT(0, color.green);
-  TEST_ASSERT_EQUAL_INT(0, color.blue);
+  TEST_ASSERT_EQUAL_INT(255, color.blue);
 }
 
 void Expect_hsv_value_to_zero_always_produce_black() {
