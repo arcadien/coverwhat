a9366191f8c3d5050880102f279f1cf3538f8c86
Aurelien Labrosse <aurelien.labrosse@gmail.com>
2019-10-28 20:43:11 +0000

    fix: Crash when thread does not finishes at time

--- software/tests/ConsoleUiTest.cpp
+++ software/tests/ConsoleUiTest.cpp
@@ -1,5 +1,4 @@
 
-#include <api/Action.h>
 #include <ui/ConsoleUi.h>
 #include <sstream>
 
@@ -19,17 +18,25 @@ void tearDown() {}
 void setUp() {}
 
 void Expect_ui_to_read_input() {
-  std::istringstream s;
-  s.str("D10\n");
-  ui::ConsoleUi ui(s);
+  int actualResult = -1;
+  {
+    std::istringstream s;
+    s.str("D10\n");
+    ui::ConsoleUi ui(s);
 
-  // give some time to the reading thread
-  // to parse the input
+    // give some time to the reading thread to effectively start
+    std::this_thread::sleep_for(
+        std::chrono::milliseconds(ui::ConsoleUi::READ_DELAY_MS * 2));
+    TEST_ASSERT(ui.ActionAvailable());
+
+    Action action = ui.GetAction();
+    actualResult = action.Process(10);
+  }
+
+  // ensure ConsoleUi thread finishes
   std::this_thread::sleep_for(
-      std::chrono::milliseconds(ui::ConsoleUi::READ_DELAY_MS * 2));
-  TEST_ASSERT(ui.ActionAvailable());
+      std::chrono::milliseconds(ui::ConsoleUi::READ_DELAY_MS));
 
-  Action action = ui.GetAction();
-  TEST_ASSERT_EQUAL_INT(0, action.Process(10));
+  TEST_ASSERT_EQUAL_INT(0, actualResult);
 }
 
